005530fbcbccc3660bb9f7cab9b65ddaf6184c7f
Cleyton Ferrari <cleytonferrari@hotmail.com>
2022-07-25 20:22:40 -0400

    atalhos

---
+++ prod/mongodb.backup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Levantando o docker
+echo "Iniciando backups..."
+date_stamp = $(date +%F-%H-%M-%S)
+echo "Criando backup - VacinaAltoParaiso"
+sudo docker exec -it mongodbvacina mongodump --db VacinaAltoParaiso --out /data/backup/$date_stamp-vacinaAltoParaiso
+echo "Criando backup - VacinaMonteNegro"
+sudo docker exec -it mongodbvacina mongodump --db VacinaMonteNegro --out /data/backup/$date_stamp-vacinaMonteNegro
+echo "Finalizado backups"
