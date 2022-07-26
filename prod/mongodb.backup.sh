bcb47b405c7482ce7926d7c45d244859d7a2779a
Cleyton Ferrari <cleytonferrari@hotmail.com>
2022-07-25 20:30:09 -0400

    hummm kkkk

--- prod/mongodb.backup.sh
+++ prod/mongodb.backup.sh
@@ -2,9 +2,12 @@
 
 # Levantando o docker
 echo "Iniciando backups..."
-date_stamp = $(date +%F-%H-%M-%S)
+date_stamp=$(date +%F-%H-%M-%S)
+
 echo "Criando backup - VacinaAltoParaiso"
 sudo docker exec -it mongodbvacina mongodump --db VacinaAltoParaiso --out /data/backup/$date_stamp-vacinaAltoParaiso
+
 echo "Criando backup - VacinaMonteNegro"
 sudo docker exec -it mongodbvacina mongodump --db VacinaMonteNegro --out /data/backup/$date_stamp-vacinaMonteNegro
+
 echo "Finalizado backups"
